e9977053e9861745838dc6904a71a507121e29de
Kayla Fang <kfang00@stuy.edu>
2020-02-27 15:16:21 -0500

    uncommented tester code

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from matrix import *
 
 screen = new_screen()
 color = [ 255, 255, 0 ]
-#m2 = []
-#add_edge(m2, 1, 2, 3, 4, 5, 6)
-#print_matrix(m2)
-#m1 = new_matrix()
-#ident(m1)
-#print_matrix(m1)
-#matrix_mult(m1, m2)
-#print_matrix(m2)
-#m = []
-#add_point(m,1,2,3)
-#add_point(m,4,5,6)
-#add_point(m,7,8,9)
-#add_point(m,10,11,12)
-#print_matrix(m)
-#matrix_mult(m, m2)
-#print_matrix(m2)
+m2 = []
+add_edge(m2, 1, 2, 3, 4, 5, 6)
+print_matrix(m2)
+m1 = new_matrix()
+ident(m1)
+print_matrix(m1)
+matrix_mult(m1, m2)
+print_matrix(m2)
+m = []
+add_point(m,1,2,3)
+add_point(m,4,5,6)
+add_point(m,7,8,9)
+add_point(m,10,11,12)
+print_matrix(m)
+matrix_mult(m, m2)
+print_matrix(m2)
 
 body = []
 add_edge(body, 120, 50, 0, 380, 50, 0)
